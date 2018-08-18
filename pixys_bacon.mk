024459e9d6fc684d40e5ea1ac66b26ec56b0b3bb
Rahul Mishra <40587889+beingmishra@users.noreply.github.com>
2018-08-19 00:12:02 +0530

    Update and rename potato_bacon.mk to pixys_bacon.mk

--- pixys_bacon.mk
+++ pixys_bacon.mk
@@ -18,10 +18,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from bacon device
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
-# Inherit from Potato vendor
-$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+# Inherit from aosp vendor
+$(call inherit-product, vendor/pixys/config/common.mk)
 
-PRODUCT_NAME := potato_bacon
+PRODUCT_NAME := pixys_bacon
 PRODUCT_DEVICE := bacon
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := A0001
